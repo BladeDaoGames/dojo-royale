d76d7da6894f645efca571808b71a00c0e37b691
0xlonescv <0xbuilderham@gmail.com>
2024-01-29 17:22:44 +0800

    :heavy_plus_sign: Add tailwind and relevant configs

--- createdojo-vite2/vite.config.ts
+++ createdojo-vite2/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from 'vite-tsconfig-paths';
 import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), wasm(), topLevelAwait()],
+    plugins: [react(), wasm(), topLevelAwait(), tsconfigPaths()],
 });
